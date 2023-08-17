66783c76f825f85c4d9d8b0678bfea9155ab385b
ssjisan <sakibfset13@gmail.com>
2023-08-18 02:10:58 +0600

    react-awsome-revel added

--- src/Layout/MenuDrawer.jsx
+++ src/Layout/MenuDrawer.jsx
@@ -1,15 +1,32 @@
-import { Box, Drawer, List, ListItem, Typography } from "@mui/material";
+import { Box, Drawer, Typography } from "@mui/material";
+import { LightClose, DarkClose, LightLogo, DarkLogo } from "../assets/Icons";
+import { menuItems } from "./MenuItems";
+import { Fade } from "react-awesome-reveal";
+
 // eslint-disable-next-line
 const MenuDrawer = ({ open, onClick, mode }) => {
     const drawerWidth = "100%";
     return (
         <Drawer anchor="right" open={open} PaperProps={{ style: { width: drawerWidth, background: mode === 'dark' ? '#121120' : '#FFFFFF' } }}>
-            <Box onClick={onClick} >
-                <List>
-                    <ListItem>
-                        <Typography color="text.1000">Hello</Typography>
-                    </ListItem>
-                </List>
+            <Box>
+                <Box sx={{ p: "16px 40px", display: "flex", justifyContent: "flex-end", alignItems: "center", cursor: "pointer" }} onClick={onClick}>
+                    {mode === "light" ? <LightClose /> : <DarkClose />}
+                </Box>
+                <Box sx={{ mt: "100px", display: "flex", justifyContent: "center" }}>
+                    {mode === "light" ? <LightLogo /> : <DarkLogo />}
+                </Box>
+                <Box sx={{ mt: "80px", mb: "80px", display: "flex", flexDirection: "column", gap: "24px" }}>
+                    <Fade top cascade duration="500">
+                        {menuItems.map((data) => {
+                            return (
+                                <Typography color="text.1000" sx={{
+                                    fontSize: "32px",
+                                    fontWeight: 700, textAlign: "center"
+                                }} key={data.id}>{data.title}</Typography>
+                            )
+                        })}
+                    </Fade>
+                </Box>
             </Box>
         </Drawer>
     );
---
+++ src/Layout/MenuItems.jsx
@@ -0,0 +1,41 @@
+
+const menuItems = [
+    {
+        id: 1,
+        title: "Home",
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "About",
+        link: "/about",
+    },
+    {
+        id: 3,
+        title: "Work Experience",
+        link: "/work-experience",
+    },
+    {
+        id: 4,
+        title: "Projects",
+        link: "/projects",
+    },
+    {
+        id: 5,
+        title: "Articles",
+        link: "/articles",
+    },
+];
+const socialMedia = [
+    {
+        id: 1,
+        title: "linkedin",
+        link: "https://www.linkedin.com/in/ssjisan/",
+    },
+    {
+        id: 2,
+        title: "github",
+        link: "https://github.com/ssjisan",
+    },
+];
+export { menuItems, socialMedia };
